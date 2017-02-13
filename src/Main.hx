a4853805bec47c9afa97ce4be89628063edaf492
sbenard <sbenard@mtwin.fr>
2017-02-13 16:53:28 +0100

    Fixed missing css update on content change

--- src/Main.hx
+++ src/Main.hx
@@ -978,6 +978,7 @@ class Main extends Model {
 			return valueHtml(c, val, sheet, obj);
 		}
 		inline function changed() {
+			updateClasses(v, c, val);
 			this.changed(sheet, c, index, old);
 		}
 		var html = getValue();
@@ -1178,7 +1179,6 @@ class Main extends Model {
 				val = !val;
 				Reflect.setField(obj, c.name, val);
 			}
-			updateClasses(v, c, val);
 			v.html(getValue());
 			changed();
 		case TImage:

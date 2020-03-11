c1011f27bd42cce1d1016cb7f67a4a50e4386fda
Iliyas Jorio <iliyas@evilempirestudio.com>
2020-03-11 14:16:02 +0100

    Fix undo forces cdb to become monofile

--- src/ops/FullSnapshot.hx
+++ src/ops/FullSnapshot.hx
@@ -2,28 +2,32 @@ package ops;
 
 class FullSnapshot implements Operation {
 	public var previousState : String;
+	public var previousFormat : String;
 	public var currentState : String;
+	public var currentFormat : String;
 
 	public function new() {
-		previousState = null;
-		currentState = null;
 	}
 
 	public function setPreviousState(context : Main) {
 		previousState = cdb.Parser.saveMonofile(context.base.data, true);
+		previousFormat = context.base.data.format;
 		return this;
 	}
 
 	public function setCurrentState(context : Main) {
 		currentState = cdb.Parser.saveMonofile(context.base.data, true);
+		currentFormat = context.base.data.format;
 	}
 
 	public function apply(context: Main) : Void {
 		context.base.loadJson(currentState);
+		context.base.data.format = currentFormat;
 	}
 
 	public function rollback(context: Main) : Void {
 		context.base.loadJson(previousState);
+		context.base.data.format = previousFormat;
 	}
 }
 
